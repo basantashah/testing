dd341825973ef440fa13ba5865918d58b2e2961e
basantashah <bindassbasanta@hotmail.com>
2019-05-28 09:13:40 +0545

    testing

---
+++ precache-manifest.23eb2bf71c4140a83541fd396ea06266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a493a99c2b842e3e497fbf655bb7f6",
+    "url": "/static/media/rStar.48a493a9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9a8948a55f59f98e58e",
+    "url": "/static/js/main.e210b7b9.chunk.js"
+  },
+  {
+    "revision": "4311620656753ef7210e",
+    "url": "/static/js/2.f2d9feaa.chunk.js"
+  },
+  {
+    "revision": "f9a8948a55f59f98e58e",
+    "url": "/static/css/main.a4642887.chunk.css"
+  },
+  {
+    "revision": "4311620656753ef7210e",
+    "url": "/static/css/2.2c6d18fb.chunk.css"
+  },
+  {
+    "revision": "0cc23ef3255f5ba772fd4ca19698bf85",
+    "url": "/index.html"
+  }
+];
